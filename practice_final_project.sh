be728bef9cbff3365e79fce6ce20bc1d1b5cffeb
Evolution-SRS <serieddine.s.r@gmail.com>
2025-06-29 23:58:04 +0300

    Create practice_final_project.sh

---
+++ practice_final_project.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This checks if the number of arguments is correct
+# If the number of arguments is incorrect ( $# != 2) print error message and exit
+if [[ $# != 2 ]]
+then
+  echo "backup.sh target_directory_name destination_directory_name"
+  exit
+fi
+
+# This checks if argument 1 and argument 2 are valid directory paths
+if [[ ! -d $1 ]] || [[ ! -d $2 ]]
+then
+  echo "Invalid directory path provided"
+  exit
+fi
+
+# [TASK 1]
+targetDirectory="$1"
+destinationDirectory="$2"
+
+echo "Target Directory =$targetDirectory"
+echo "Destination Directory =$destinationDirectory"
